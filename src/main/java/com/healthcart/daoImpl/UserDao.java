8f4b956a7c96f468622d3ff58990cb57413dbf5c
rahul7284 <rahulpyasi42@gmail.com>
2017-12-08 22:44:03 +0530

    ok

--- src/main/java/com/healthcart/daoImpl/UserDao.java
+++ src/main/java/com/healthcart/daoImpl/UserDao.java
@@ -63,16 +63,9 @@ public class UserDao implements UserDaoInterface {
 	}
 */
 	
-	@Override
-	public Integer saveUser(UserModel user) {
-		session = sessionFactory.openSession();
-		txn = session.beginTransaction();
-		int save = (int) session.save(user);
-		txn.commit();
-		session.close();
-		return save;
-	}
-*/
+	
+
+
 	@Override
 	public Integer saveUser(UserModel user) {
 		session = sessionFactory.openSession();
